cdef5244913838752fd4440f2f2f129552f44528
singhrupa605 <singhrupa605@gmail.com>
2022-03-09 17:46:54 +0530

    ME_QTRIPDYNAMIC_MODULE_ADVENTURE_DETAILS  (Crio.Do)

--- frontend/conf/index.js
+++ frontend/conf/index.js
@@ -1,4 +1,4 @@
 
-const config = { backendEndpoint: "http://3.7.151.205:8082" };
+const config = { backendEndpoint: "http://15.207.155.118:8082" };
 
 export default config;
--- frontend/modules/adventure_details_page.js
+++ frontend/modules/adventure_details_page.js
@@ -4,19 +4,24 @@ import config from "../conf/index.js";
 function getAdventureIdFromURL(search) {
   // TODO: MODULE_ADVENTURE_DETAILS
   // 1. Get the Adventure Id from the URL
-
-
+ 
   // Place holder for functionality to work in the Stubs
-  return null;
+  return search.split("=")[1];
 }
 //Implementation of fetch call with a paramterized input based on adventure ID
 async function fetchAdventureDetails(adventureId) {
   // TODO: MODULE_ADVENTURE_DETAILS
   // 1. Fetch the details of the adventure by making an API call
-
+  try{
+    const url = config.backendEndpoint + `/adventures/detail/?adventure=${adventureId}`;
+      const apiData= await fetch(url);
+      const jsonData = await apiData.json();
+      return jsonData;
+    } catch (err) {
+      return null;
+    }
 
   // Place holder for functionality to work in the Stubs
-  return null;
 }
 
 //Implementation of DOM manipulation to add adventure details to DOM
@@ -24,12 +29,61 @@ function addAdventureDetailsToDOM(adventure) {
   // TODO: MODULE_ADVENTURE_DETAILS
   // 1. Add the details of the adventure to the HTML DOM
 
+  let heading = document.getElementById("adventure-name");
+  heading.textContent = adventure.name;
+
+  let subtitle = document.getElementById("adventure-subtitle");
+  subtitle.textContent = adventure.subtitle;
+
+  let cardContainer= document.getElementById("photo-gallery");
+
+  //adding images to photo-gallery;
+  let images = adventure.images
+  images.forEach(image=>
+    {
+      let cardImg = document.createElement("img");
+      cardImg.src = image;
+      cardImg.className = "activity-card-image";
+      cardContainer.append(cardImg);
+    })
+    let cardContent = document.getElementById("adventure-content");
+    cardContent.textContent = adventure.content;
+  
 }
 
 //Implementation of bootstrap gallery component
 function addBootstrapPhotoGallery(images) {
   // TODO: MODULE_ADVENTURE_DETAILS
   // 1. Add the bootstrap carousel to show the Adventure images
+
+  let carouselContainer = document.getElementById("photo-gallery");
+  carouselContainer.id = "carouselExampleIndicators";
+  
+  //Removing older pictures added in addAdventureDetailsToDOM(adventure) method
+   let oldImages =   carouselContainer.getElementsByClassName("activity-card-image");
+ 
+    Array.from(oldImages).forEach(image=>
+      {
+        image.remove();
+      })
+
+  // Creating new Carousel picture gallery
+  let innerCarousel = document.getElementById("inner");
+ 
+  images.forEach(image=>{
+      let carouselItem = document.createElement("div");
+      carouselItem.className = "carousel-item";
+
+      let cardImg = document.createElement("img");
+      cardImg.src = image
+      cardImg.className = "activity-card-image";
+
+      carouselItem.append(cardImg);
+       innerCarousel.append(carouselItem);
+  })
+  
+    innerCarousel.firstChild.classList.add("active");
+    carouselContainer.append(innerCarousel);
 
 }
 
